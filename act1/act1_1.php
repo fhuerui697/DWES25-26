b4e9c0ea0783a1b36a0d73830a6b9f588a9dc396
fhuerui697 <fhuerui697@g.educaand.es>
2025-09-28 00:27:39 +0200

    primer commit

---
+++ act1/act1_1.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba PHP</title>
+</head>
+<body>
+    <b>Saludo:</b> 
+    <?php echo "Hola, mundo!"; ?>
+</body>
+</html>
+
