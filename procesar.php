a204271dbb31d422d552e22ab3741720ce397919
Federico Huercano Ruiz <fhuerui697@g.educaand.es>
2025-10-15 09:30:59 +0200

    Add files via upload

--- procesar.php
+++ procesar.php
@@ -23,7 +23,8 @@
     $lenguajes_permitidos = ['html', 'css', 'javascript', 'php'];
     if (isset($_POST['lenguajes']) && is_array($_POST['lenguajes']))
     {
-        if (empty(array_diff($_POST['lenguajes'], $lenguajes_permitidos)))
+        $no_validos = array_diff($_POST['lenguajes'], $lenguajes_permitidos);
+        if (count($no_validos) == 0)
             echo 'Los lenguajes recibidos están dentro de los esperados y son: '.implode(', ',$_POST['lenguajes']);      
             else
                 echo 'Se han recibido lenguajes no esperados';
@@ -36,7 +37,8 @@
     $habilidades_permitidas = ['ux', 'bbdd', 'git', 'seo'];
     if (isset($_POST['habilidades']) && is_array($_POST['habilidades']))
     {
-        if (empty(array_diff($_POST['habilidades'], $habilidades_permitidas)))
+        $no_validos = array_diff($_POST['habilidades'], $habilidades_permitidas);
+        if (count($no_validos) == 0)
             echo 'Las habilidades recibidas están dentro de los esperadas y son: '.implode(', ',$_POST['habilidades']);      
             else
                 echo 'Se han recibido habilidades no esperadas';
@@ -46,44 +48,41 @@
 
     echo "<br>";
     // Procesamiento de ficheros múltiples
-    // Definir carpeta de destino
+    // Carpeta de destino
+$destDir = __DIR__ . '/uploads';
+if (!is_dir($destDir)) {
+    mkdir($destDir, 0775, true);
+}
 
-    $destDir = __DIR__ . '/uploads';
-    if (!is_dir($destDir)) {
-        mkdir($destDir, 0775, true);
-    }
+// Límite y tipos permitidos
+const MAX_SIZE = 100000;
+$tiposPermitidos = ['image/png'];
 
-    // Límite y tipos permitidos
-    const MAX_SIZE = 30000;
-    $tiposPermitidos = ['image/png'];
+// Comprobar si se ha subido un archivo
+if (isset($_FILES['archivo'])) {
 
-    // Comprobar si se han subido archivos
-    if (isset($_FILES['archivo'])) {
+    $nombre = $_FILES['archivo']['name'];
+    $tmp    = $_FILES['archivo']['tmp_name'];
+    $error  = $_FILES['archivo']['error'];
+    $size   = $_FILES['archivo']['size'];
 
-        // Recorremos todos los archivos subidos
-        foreach ($_FILES['archivo']['name'] as $i => $nombre) {
-            $tmp   = $_FILES['archivo']['tmp_name'][$i];
-            $error = $_FILES['archivo']['error'][$i];
-            $size  = $_FILES['archivo']['size'][$i];
-
-            if ($error !== UPLOAD_ERR_OK) {
-                echo "Error con $nombre (código $error).<br>";
-            } else if ($size > MAX_SIZE) {
-                echo "$nombre: supera el tamaño máximo.<br>";
-            } else if (!is_uploaded_file($tmp)) {
-                echo "$nombre: subida no válida.<br>";
-            } else if (!in_array(mime_content_type($tmp), $tiposPermitidos, true)) {
-                echo "$nombre: tipo no permitido.<br>";
-            } else {
-                $destino = $destDir . '/' . basename($nombre);
-                if (move_uploaded_file($tmp, $destino)) {
-                    echo "Subido: $nombre<br>";
-                } else {
-                    echo "No se pudo mover $nombre.<br>";
-                }
-            }
+    if ($error !== UPLOAD_ERR_OK) {
+        echo "Error con $nombre (código $error).<br>";
+    } elseif ($size > MAX_SIZE) {
+        echo "$nombre: supera el tamaño máximo.<br>";
+    } elseif (!is_uploaded_file($tmp)) {
+        echo "$nombre: subida no válida.<br>";
+    } elseif (!in_array(mime_content_type($tmp), $tiposPermitidos, true)) {
+        echo "$nombre: tipo no permitido.<br>";
+    } else {
+        $destino = $destDir . '/' . basename($nombre);
+        if (move_uploaded_file($tmp, $destino)) {
+            echo "Subido: $nombre<br>";
+        } else {
+            echo "No se pudo mover $nombre.<br>";
         }
     }
+}
     
     // Procesar el dato oculto
     if (isset ($_POST['dato_oculto']))
